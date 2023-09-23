aa30e9305511fa57e3a18734a5aac05aab635d0b
mbobic1 <mbobic1@etf.unsa.ba>
2023-09-23 15:12:39 +0200

    student

--- nastava/src/components/screens/StudentScreen.jsx
+++ nastava/src/components/screens/StudentScreen.jsx
@@ -1,8 +1,29 @@
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/@/components/ui/card"
 
 const StudentScreen = () => {
     return(
-        <div>uslo ovdje</div>
+        <div className="flex-row justify-around flex mb-5" style={{display: 'block'}}>
+            <h1>Dostupni testovi za rad</h1>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+      </div>
     )
 }
 
