02d030ab5f2973d232d98fd84366a158be4be0e4
Nail Bobić <92312723+nbobic1@users.noreply.github.com>
2024-12-28 12:58:32 +0100

    Update migration.js

--- backend/migration.js
+++ backend/migration.js
@@ -1,10 +1,10 @@
 const createTable = [
-" CREATE TABLE usertable (id int4 NOT NULL, username varchar NOT NULL, passwordhash varchar NOT NULL, userrole varchar NOT NULL);",
-  " CREATE TABLE tests (id int4 NOT NULL, username varchar NOT NULL, answers json NOT NULL, question json NOT NULL, testdate json NOT NULL, minutes int4 NOT NULL, title varchar NOT NULL);",
-  " CREATE TABLE startedtest (id int4 NOT NULL, username varchar NOT NULL, startedtest int4 NOT NULL, testid int4 , bodovi float8 );",
-  " CREATE TABLE results (id int4 NOT NULL, username varchar NOT NULL, points int4 NOT NULL, nazivtesta varchar , idtesta int4 NOT NULL);",
-  " CREATE TABLE questions (id int4 NOT NULL, group_id int4 NOT NULL, points int4 NOT NULL, negativepoints int4 NOT NULL, qtext varchar NOT NULL, answers varchar NOT NULL, question varchar NOT NULL);",
-" CREATE TABLE grouptable (id int4 NOT NULL, groupname varchar NOT NULL, username varchar NOT NULL);"    
+" CREATE TABLE usertable (id SERIAL, username varchar , passwordhash varchar, userrole varchar );",
+  " CREATE TABLE tests (id SERIAL, username varchar , answers json , question json, testdate json , minutes int4 , title varchar );",
+  " CREATE TABLE startedtest (id SERIAL, username varchar , startedtest int4 , testid int4 , bodovi float8 );",
+  " CREATE TABLE results (id SERIAL, username varchar , points int4, nazivtesta varchar , idtesta int4 );",
+  " CREATE TABLE questions (id SERIAL, group_id int4 , points int4 , negativepoints int4 , qtext varchar , answers varchar , question varchar);",
+" CREATE TABLE grouptable (id SERIAL, groupname varchar , username varchar );"    
 ]
 const express = require("express");
 const app = express();
